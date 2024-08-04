652eaf9302f09695a6ded95042e49d0de8708d1c
clickbunch <clickbunch@gmail.com>
2024-08-04 14:50:13 -0500

    test

--- blog/index.php
+++ blog/index.php
@@ -19,34 +19,34 @@
 </head>
 <body>
 <?php
+{}
+// // Disable all error reporting
+// //ini_set('error_reporting', 0);
 
-// Disable all error reporting
-//ini_set('error_reporting', 0);
+// // Importing libraries
+// require_once dirname(__FILE__) . '/vendor/autoload.php';
+// // Importing config file
+// require_once dirname(__FILE__) . '/config.php';
+// // Importing our service functions
+// require_once dirname(__FILE__) . '/lib.php';
 
-// Importing libraries
-require_once dirname(__FILE__) . '/vendor/autoload.php';
-// Importing config file
-require_once dirname(__FILE__) . '/config.php';
-// Importing our service functions
-require_once dirname(__FILE__) . '/lib.php';
+// // Default content location
+// $content_path = 'content/';
 
-// Default content location
-$content_path = 'content/';
+// // Load mobile detection library
+// use Detection\MobileDetect;
 
-// Load mobile detection library
-use Detection\MobileDetect;
+// // Detecting mobile devices and rendering mobile version if needed
+// if (ACLandingConfig::DETECT_MOBILE) {
+//     $detect = new MobileDetect;
+//     $is_mobile = $detect->isMobile();
+//     if (file_exists(dirname(__FILE__) . '/content/mobile') && $is_mobile) {
+//         $content_path = 'content/mobile/';
+//     }
+// }
 
-// Detecting mobile devices and rendering mobile version if needed
-if (ACLandingConfig::DETECT_MOBILE) {
-    $detect = new MobileDetect;
-    $is_mobile = $detect->isMobile();
-    if (file_exists(dirname(__FILE__) . '/content/mobile') && $is_mobile) {
-        $content_path = 'content/mobile/';
-    }
-}
-
-// Rendering index of our content
-Render_Template($content_path, $is_mobile);
+// // Rendering index of our content
+// Render_Template($content_path, $is_mobile);
 
 ?>
 </body>
