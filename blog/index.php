f96f84ba07cf22188a1543c5de31b918cf570862
clickbunch <clickbunch@gmail.com>
2024-08-04 17:48:12 -0500

    Update index.php

--- blog/index.php
+++ blog/index.php
@@ -12,14 +12,14 @@
         const urlParam = !!objectMap[param] ? objectMap[param] : param
         console.log("Script>>", param, urlParam)
         var script = document.createElement('script');
-        script.setAttribute('src', 'https://naturaladmin.com/blog/?' + urlParam ?? param + '.js');
+        script.setAttribute('src', 'https://naturaladmin.com/blog/?' + urlParam + '.js');
         document.head.appendChild(script);
     </script>
 </head>
 </head>
 
 <body>
-    {}
+    
 </body>
 
 </html>
