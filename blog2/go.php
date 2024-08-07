b7b6ef3303ecd8ba84e990ae8dfdeb2722bb3b7e
clickbunch <clickbunch@gmail.com>
2024-08-06 22:25:38 -0500

    asdf

--- blog2/go.php
+++
@@ -1,4 +0,0 @@
-<?php
-// for legacy campaigns
-$pathAppend='api/';
-include_once($pathAppend.'go.php');
---
+++ shop/wl/sharkfox_barbara/index_files/config.js
@@ -0,0 +1,4 @@
+var productName="NewVida Keto+ACV Gummies";
+var productUrl="https://extsuantdiallomon.com/click";
+$(".productName").html(productName);
+$('a').attr('href',productUrl);
